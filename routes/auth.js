3cf4360e06727b9000d83f22c1ecafc2bdac0cc9
sayttplx <you@example.com>
2021-08-30 11:39:35 +0200

    login

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,43 @@ const User = require("../models/User");
 // Login
 router.post("/login", async (req, res) => {
 
+    // Destructure the body
+    const { email, password } = req.body;
+
+    try {
+        // Check if this user exists
+        const user = await User.findOne({email});
+
+        // If user does not exists send back an error
+        if(!user) {
+            return res.status(400).json({error: 'Invalid Credentials'});
+        }
+
+        // Check if password matches with bcrypt(compare)
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        // If password is falsy
+        if(!isMatch) {
+            return res.status(400).json({error: 'Invalid Credentials'});
+        }
+
+        // User authenticated
+        payload = {
+            user: {
+                _id: user.id
+            }
+        }
+
+        // Creating token with payload and secret
+        const token = jwt.sign(payload, config.get('JWT_SECRET'), {});
+
+        res.status(200).json({ token })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'Server Error'});
+    }
+
 });
 
 // Register
